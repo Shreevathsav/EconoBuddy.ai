542650aeaeaf3bca8f5f9ad96c28f62eb8392735
Shreevathsav <58297203+Shreevathsav@users.noreply.github.com>
2025-04-28 22:30:29 +0530

    spring boot init

---
+++ SpringBoot-backend/econobuddy/src/main/java/com/shree/econobuddy/EconobuddyApplication.java
@@ -0,0 +1,13 @@
+package com.shree.econobuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconobuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconobuddyApplication.class, args);
+	}
+
+}
